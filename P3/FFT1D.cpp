f86df53adc9d3bdd169918bc7548ec361b42c64d
Grant Hooks <granthooks28@gmail.com>
2018-10-23 21:15:50 -0700

    Read in functions, have all 3, need to check shift

--- P3/FFT1D.cpp
+++ P3/FFT1D.cpp
@@ -8,11 +8,52 @@ using namespace std;
 
 void fft(float data[], unsigned long nn, int isign);
 void PrintFunction(float function[], int nn, string output);
+void ApplyFFT(float function[], int N, string Output);
+void SampleCos();
 
 int main(int argc, char *argv[])
 {
   float function[9] = {-1, 2, 0, 3, 0, 4, 0, 4, 0};
-  int N = 4;
+
+  ApplyFFT(function, 4, "Function1.txt");
+
+  //Function 2 - Cosine function
+  float function2[257];
+
+  ifstream fin("cosFunction.txt");
+  for(int i = 1; i < 257; i += 2) {
+    fin >> function2[i];
+
+    //shift to center
+    function2[i] *= pow(-1, i-1);
+  }
+  fin.close();
+  for(int i = 0; i < 257; i += 2) {
+    function2[i] = 0;
+  }
+
+  ApplyFFT(function2, 128, "Function2.txt");
+
+  //Function 3 - Rectangular function
+  float function3[257];
+
+  fin.open("Rect_128.dat");
+  for(int i = 1; i < 257; i += 2) {
+    fin >> function3[i];
+
+    //shift to center
+    function3[i] *= pow(-1, i-1);
+  }
+  for(int i = 0; i < 257; i += 2) {
+    function3[i] = 0;
+  }
+  fin.close();
+
+  ApplyFFT(function3, 128, "Function3.txt");
+
+}
+
+void ApplyFFT(float function[], int N, string Output) {
   int arrayLength = N * 2 + 1;
 
   //forward fft
@@ -23,30 +64,32 @@ int main(int argc, char *argv[])
     function[i] /= N;
   }
 
-  PrintFunction(function, N, "Function1.txt");
-
-  //inverse fft
-  cout << "Inverse: ";
-  fft(function, 4, 1);
-
-  for(int i = 1; i < arrayLength; i++) {
-    cout << function[i] << ", ";
-  }
-
-  cout << endl;
+  PrintFunction(function, N, Output);
 }
 
+//Samples cosine function and prints as text
+void SampleCos() {
+  ofstream file("cosFunction.txt");
+  float pi = 3.14;
+  for(int i = 0; i < 128; i++) {
+    float val = cos((2 * pi * 8 * i)/128);
+    file << val << endl;
+  }
+  file.close();
+}
+
+//prints function's real, imaginary, and magnitude values
 void PrintFunction(float function[], int nn, string output) {
   ofstream file(output);
   int arrayLength = nn * 2 + 1;
 
-  //print total
+  /*//print total
   file << "Total: " << endl;
   for(int i = 1; i < arrayLength; i++) {
     file << function[i] << endl;
   }
 
-  file << endl;
+  file << endl;*/
 
   //print real
   file << "Real: " << endl;
@@ -54,7 +97,7 @@ void PrintFunction(float function[], int nn, string output) {
     file << function[i] << endl;
   }
 
-  file << endl;
+  file << endl << endl;
 
   //print imaginary
   file << "Imaginary: " << endl;
@@ -62,7 +105,7 @@ void PrintFunction(float function[], int nn, string output) {
     file << function[i] << endl;
   }
 
-  file << endl;
+  file << endl << endl;
 
   //print magnitude
   file << "Magnitude: " << endl;
@@ -71,7 +114,7 @@ void PrintFunction(float function[], int nn, string output) {
     file << magnitude << endl;
   }
 
-  file << endl;
+  file << endl << endl;
 
   file.close();
 }
